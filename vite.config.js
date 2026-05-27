e7fb783028e38aa28c5fa7428534ac9be8437830
HX <90646662+HXakaXinyufan@users.noreply.github.com>
2026-05-27 17:43:51 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import autoprefixer from "autoprefixer";
 
 export default defineConfig({
+  base: "/wavsorterV2/", // <--- This is the crucial addition for GitHub Pages
   root: ".",
   build: {
     outDir: "dist",
