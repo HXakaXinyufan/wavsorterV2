3a590f9c7d5a0e85ae809efab5854174cc256e02
HX <90646662+HXakaXinyufan@users.noreply.github.com>
2026-05-27 18:02:59 +0200

    Update member-data.js

--- assets/member-data.js
+++ assets/member-data.js
@@ -1,314 +1,97 @@
 export const memberData = {
-  SeoYeon: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/a5dd9af2-dbc5-42c8-11c3-6c6cdfc05d00/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/724d67d5-052b-4aeb-1ad8-73bf9501f600/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/c786560a-5e02-47ca-7c33-9b4c55b36f00/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/8e5dd8ef-8de1-422a-2963-a4d6d2352d00/2x",
-    color: "#21AEFE",
-    emoji: "🐶",
-    sNumber: "S1",
-  },
-  HyeRin: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/d7a53eda-1fb2-4544-4e48-c8e92e7e5800/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/9af3bb14-81f3-4188-9aa5-279e50ae8e00/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/8d802e08-0c7e-4961-4a0f-1e2cf2645300/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/710bca4c-0b5e-4e7f-f961-e46536f6b800/2x",
-    color: "#830CE8",
-    emoji: "🐱",
-    sNumber: "S2",
-  },
-  JiWoo: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/c5313971-4780-4b13-0809-340a9eb24300/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/20b831bf-af88-489e-c9e0-907e5d7f9200/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/370a1e77-c63c-4508-17de-2f6345ce9900/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/fe964236-1dd2-4957-5908-08d02c1e1c00/2x",
-    color: "#FDF70A",
-    emoji: "🐻",
-    sNumber: "S3",
-  },
-  ChaeYeon: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/f74619cd-0e62-4d0d-273d-a5267432bf00/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/685012bd-00f3-4006-0a0a-561700890400/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/a43959b5-3589-4d87-3d09-2f564cd4d100/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/33ab15f9-650f-4879-9a5f-45a03bc35000/2x",
-    color: "#98C64A",
-    emoji: "🦌",
-    sNumber: "S4",
-  },
-  YooYeon: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/e9761b20-8167-4a46-3685-cbd9979c3000/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/237560f3-0166-4f14-2f4f-ce7c99498700/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/aa3df8a8-87dd-4e43-e4e6-4106af915100/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/037ec0d8-bbd4-4103-8086-c8ac2bf5e700/2x",
-    color: "#DC0B74",
-    emoji: "🐰",
-    sNumber: "S5",
-  },
-  SooMin: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/365eafd5-9dd7-4ae6-5e56-e37f6e48a900/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/2fe12f98-6951-42e2-75e2-7613595fd200/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/e8ff5769-3b7c-44db-f473-d40a39b6c800/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/11c5115b-c056-4013-420e-edee83860000/2x",
-    color: "#F985A4",
-    emoji: "🐿️",
-    sNumber: "S6",
-  },
-  NaKyoung: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/47342288-3e4b-4f87-0631-47469e3ae600/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/ce56f6d6-868c-4925-dc85-c9d6cbec9700/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/99b8e351-70e2-44db-26b0-d7b8b5d80800/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/fa19baf4-45c8-4612-a2ba-5c01ed4b9400/2x",
-    color: "#659AA0",
-    emoji: "🐈",
-    sNumber: "S7",
-  },
-  YuBin: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/98a11cf5-49e6-40ff-0740-3f8af1a34c00/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/3cc319fc-bcd7-4dc0-8833-871553eb6000/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/ceb24d4d-d390-4c9d-c48c-6ce494e82100/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/83584fd6-18c7-46b3-69e4-6c5d7877fd00/2x",
-    color: "#FDE4E0",
-    emoji: "🐯",
-    sNumber: "S8",
-  },
-  Kaede: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/cf32327b-0381-4790-6061-2441a8c75400/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/481894b2-2697-4256-2fd2-e009f1d8ec00/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/ea46c6e5-e02f-4577-04d3-bfa0dc8be200/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/edb1c31c-a96a-4b0c-eef4-2387dbab0600/2x",
-    color: "#FDB634",
-    emoji: "🍁",
-    sNumber: "S9",
-  },
-  DaHyun: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/fac5f0f1-a638-499d-8d43-a86df6c92400/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/cdf62bb7-4395-427a-ad20-ff7d7a2e5900/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/4d85285b-656d-4d24-a4c3-603af182e800/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/fdc4b9d9-eb33-4695-82c5-01e086fa5c00/2x",
-    color: "#FE9BD6",
-    emoji: "🍒",
-    sNumber: "S10",
-  },
-  Kotone: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/993472dc-d43f-4775-1e98-27e2f72b7400/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/fe35ec53-4cfe-4e5e-2e2c-3a47e6697000/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/13eaaea9-c89c-44d1-9120-904029ff1300/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/58aeb105-9ac4-4fa9-d4c1-f4939607f900/2x",
-    color: "#E3C500",
-    emoji: "🦭",
-    sNumber: "S11",
-  },
-  YeonJi: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/cbfc59d3-12bf-443e-f528-300c790aac00/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/6cc5b006-d76a-4ba7-5f88-1739571b4000/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/e10be72d-9481-480d-c667-6ba544809000/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/9ab313f5-8c1f-4d46-1154-63d777edd800/2x",
-    color: "#5974FA",
-    emoji: "🦆",
-    sNumber: "S12",
-  },
-  Nien: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/42f626f9-7043-404a-2797-83b983284900/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/a790c56a-4ec2-4673-3512-e93c03c57500/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/6af8feb1-91fe-4d46-712d-55471ab05300/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/1137f5da-0ffd-48af-0900-782d49d03d00/2x",
-    color: "#FD963D",
-    emoji: "🐑",
-    sNumber: "S13",
-  },
-  SoHyun: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/84d33420-2161-437d-de0f-ad759fcf0d00/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/dcc69be0-fa86-4b34-fe1b-bca31b646800/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/141fc129-dab4-4025-603b-efb04556da00/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/163b1e2a-2816-45fc-c975-8d1ec0f35f00/2x",
-    color: "#1223B2",
-    emoji: "🐺",
-    sNumber: "S14",
-  },
-  Xinyu: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/a1bf1fe5-7e0f-44b0-a85e-0553e4896300/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/c5ecd178-eb9a-45a2-446d-bd0ff8b79c00/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/c72ff987-b794-4c51-ecc3-3756fd1d9900/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/2354b973-75f9-4f7d-39be-90318cb60000/2x",
-    color: "#CA1E20",
-    emoji: "🦊",
-    sNumber: "S15",
-  },
-  Mayu: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/441c97b7-2691-4d57-23cf-44ad9a9c9600/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/fd453a3d-7ca3-493a-2549-d8bacdf07300/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/97b3b326-dbfa-483a-6aa4-cbcc45248300/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/4f431c3b-dad6-4a0d-c4c1-8243968ef800/2x",
-    color: "#FB9074",
-    emoji: "🐇",
-    sNumber: "S16",
-  },
-  Lynn: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/3b72ca8b-6f41-44aa-af59-afac4962d800/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/6551ea9e-a593-4bb7-97ef-b2694dfd6700/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/d822361f-1e5e-4d22-2def-b9369bfaff00/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/34f4fe71-8dc1-4b0a-4ec2-484827eea300/2x",
-    color: "#AC60B8",
-    emoji: "🦈",
-    sNumber: "S17",
-  },
-  JooBin: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/93d241c4-8c42-4467-5e59-92835b1fbf00/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/66308f0c-f2e7-426c-5555-8c6d819fef00/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/ac23e972-6b1f-4c4e-0fa2-5d763bd93800/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/1cec4828-2f94-45fa-b7f9-62156aeec700/2x",
-    color: "#B7F54C",
-    emoji: "🐣",
-    sNumber: "S18",
-  },
-  HaYeon: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/ad6ed4bb-8351-4dc7-d907-552bf9577400/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/63e5e54b-3582-43af-d1b4-2243d94e9f00/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/85615917-beff-4983-3615-a7f06bccf900/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/92239921-7157-407a-7941-646f1eb77000/2x",
-    color: "#52D8BB",
-    emoji: "🐹",
-    sNumber: "S19",
-  },
-  ShiOn: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/19d79da5-ce06-42c1-f35c-3cda1ea35300/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/7a35d0bc-5498-4e92-1019-99e8a0bca900/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/9a5c34a6-0f60-4e8a-830c-720c5d947700/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/3263ded0-6aac-41d1-e160-f5a757a38200/2x",
-    color: "#FF428A",
-    emoji: "🍞",
-    sNumber: "S20",
-  },
-  ChaeWon: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/561c1f67-dec0-41d1-2a3f-99eec9b5df00/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/3bc11e96-a4ae-45f5-cf76-c503d9395b00/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/c5c972d8-5753-4bfe-8418-ccc33e137600/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/2ca84f79-654d-449c-ee40-d78b89d1b200/2x",
-    color: "#C3A4E0",
-    emoji: "🎀",
-    sNumber: "S21",
-  },
-  Sullin: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/2bfeb185-6e27-4bef-5989-4f6e3b6bc900/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/d2b7fb3a-5365-4c58-ccf0-824918eba700/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/7dc8e475-a910-4a5a-d9dd-0e06442c5200/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/1b24034a-ed85-448e-02ed-8936eb974e00/2x",
-    color: "#7DBA8D",
-    emoji: "⛄️",
-    sNumber: "S22",
-  },
-  SeoAh: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/223c49a4-a189-4732-d69b-a2aad8c07a00/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/7a77aac7-3745-48e1-ea52-01de5662ae00/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/6c67ed45-8c6a-4534-1ff0-656a3edf1b00/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/b36f2b49-90c0-4c0d-471d-62f1d8498a00/2x",
-    color: "#D0F3FF",
-    emoji: "☀️",
-    sNumber: "S23",
-  },
-  JiYeon: {
-    picSet1:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/cb9c4823-dbe6-4abc-b755-6eb53efba500/2x",
-    picSet2:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/b667bb40-bdef-4fa2-ec86-01277c3edd00/2x",
-    picSet3:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/52143d1f-43a0-4d44-8bde-036febd47300/2x",
-    picSet4:
-      "https://imagedelivery.net/qQuMkbHJ-0s6rwu8vup_5w/5b9dc16b-38b4-4f4f-be23-44c1518a6e00/2x",
-    color: "#FFAB64",
-    emoji: "🦢",
-    sNumber: "S24",
-  },
+  "@gaeulmaplefall (faris, cringewav)": {},
+  "@4everlikethestarss (p00pyh34d)": {},
+  "@yooyeon.zip (yooenmimi)": {},
+  "@adoreelina": {},
+  "@sssomday (chudrian)": {},
+  "@aikofx": {},
+  "@ppotatozs (Xinsohyun)": {},
+  "@nyasfaveouritespear (AScension)": {},
+  "@kaesfavwav (910yurilover)": {},
+  "@wheretheheckisbaki (Baki)": {},
+  "@kamimotoprincess (hiHeyD)": {},
+  "@mechamaru3508 (known as fish)": {},
+  "@sullinsized (bri)": {},
+  "@iyooooooooooooooo (baby peter)": {},
+  "@cheannie (Nikari)": {},
+  "@chocqs (choco)": {},
+  "@markdadark": {},
+  "@danah": {},
+  "@ilovegirlgroups (dia)": {},
+  "@diumend (S4 win S4)": {},
+  "@hello.5007 (hyerin will take over the world)": {},
+  "@heyitsmedxve": {},
+  "@garnetz (Garnet)": {},
+  "@hannnnnaaaaaa (Id : Hannnaa)": {},
+  "@highkwakeneuinely": {},
+  "@itsmebuu (Bononana)": {},
+  "@izzu._. (itsuizzu)": {},
+  "@jackfrombuffalo (JackJiwoo)": {},
+  "@seoyeon.zip (jamjam)": {},
+  "@jcnoof": {},
+  "@wavline.24 (jellyrin)": {},
+  "@fuecocoishere (JiwooSoup)": {},
+  "@juneisforjuline": {},
+  "@winterville119 (kj)": {},
+  "@tthyybaauut (Kwakamole)": {},
+  "@jeremyspokein (kyotone)": {},
+  "@liang8601": {},
+  "@mady0603 (LUVMADY)": {},
+  "@mac995223 (Mac99)": {},
+  "@mahimahivii": {},
+  "@nxtglow (boblynn)": {},
+  "@24marcosmos": {},
+  "@marunisa": {},
+  "@tripleseu (mei)": {},
+  "@mizi22 (Michelle)": {},
+  "@min_ju01 (앙님)": {},
+  "@auswav_wizone (MochizLuvr)": {},
+  "@mooxhan": {},
+  "@myshion_ (mayki)": {},
+  "@ny4ngky": {},
+  "@._nguyenel_. (your GA Weaner)": {},
+  "@nicorai484_": {},
+  "@niqxen (Niqxy)": {},
+  "@13okumura": {},
+  "@pandu.22 (pandsosul)": {},
+  "@dogsdaysareover (pdg)": {},
+  "@emc21161 (Queti)": {},
+  "@requie_m": {},
+  "@rezh": {},
+  "@sawwwyy (Sil)": {},
+  "@skysmores": {},
+  "@socialpots": {},
+  "@j6039_11156 (Spearmintz)": {},
+  "@tama_got.chi (Hoon)": {},
+  "@leoleow_": {},
+  "@sinubok (Tested)": {},
+  "@tickymeister (CardinH)": {},
+  "@tzushin": {},
+  "@tzuwi_. (xintzuwi)": {},
+  "@wtrmllon": {},
+  "@bagelpizzas (mike)": {},
+  "@ak0ru": {},
+  "@robo_lija": {},
+  "@orchid_from_sg": {},
+  "@wav_HX": {},
+  "@_red._ (LoveS13)": {},
+  "@chirakshee.wav (chiro)": {},
+  "@cossy": {},
+  "@noshi.aep": {},
+  "@imkyron (NaKyNada)": {},
+  "@dearseoah": {},
+  "@satzumiii (Koshark)": {},
+  "@pepperonitomato (아프지마유)": {},
+  "@sefrob (sefrobbian)": {},
+  "@9muses (iD: S2UH2H)": {},
+  "@thesalty.one (hoplegance kim s25 || kosmos11)": {},
+  "@afterseaa": {},
+  "@on_ion_pond (onion)": {},
+  "@noobiex16 (NewbieX2)": {},
+  "@_chy24 (Caitlin)": {},
+  "@chaezgarden (sailorseo)": {},
+  "@aizenero (weiz)": {},
+  "@falling_infall (oblirator)": {},
+  "@haewon.z (doyaz)": {},
+  "@sammifest (triplesammi)": {}
 };
