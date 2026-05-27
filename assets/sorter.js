ec9b1a5e6a0ed3c01a261712de4e8cddc25dc0c4
HX <90646662+HXakaXinyufan@users.noreply.github.com>
2026-05-28 00:35:30 +0200

    Update sorter.js

--- assets/sorter.js
+++ assets/sorter.js
@@ -7,7 +7,7 @@ const html = (strings, ...values) =>
 const memberNames = Object.keys(memberData);
 let sorter = new TripleSBiasSorter(memberNames, memberData);
 
-const FLIP_TRANSITION_MS = 100;
+const FLIP_TRANSITION_MS = 200;
 
 const SUN_SVG =
   '<path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path>';
